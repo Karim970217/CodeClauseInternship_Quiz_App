110155a711434db19b5f924ae10e114cd0c93c09
karim <karim@example.com>
2025-03-02 16:39:10 +0530

    responsive

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -57,8 +57,8 @@ const Quiz = () => {
     }
 
   return (
-    <div className='h-[100vh] flex justify-center items-center'>
-        <div className='flex flex-col w-[80%] h-[90%] md:w-[60%] md:h-[80%] bg-[#6832a8] rounded-lg border-3 border-black-600'>
+    <div className='h-screen flex justify-center items-center'>
+        <div className='flex flex-col w-[80%] md:w-[60%] h-[80%] bg-[#6832a8] rounded-lg border-3 border-black-600'>
             <h1 className='text-[30px] flex justify-center text-white font-bold'>Quiz App</h1>
             {result?
             <>
